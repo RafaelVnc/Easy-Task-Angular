a9451c82c5c8320fc737703f0d2df80dbffcb835
Rafael Venancio <rafael.venancio08.06@gmail.com>
2026-03-17 15:41:04 -0300

    user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,33 @@
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+    @Input({ required: true }) id!: string;
+    @Input({ required: true }) avatar!: string;
+    @Input({ required: true }) name!: string;
+    @Output() select = new EventEmitter<string>(); // Pass the information that a specific user was selected to the component that is using UserComponent
+
+    get imagePath() {
+        return 'assets/users/' + this.avatar;
+    }
+  
+    onSelectUser() {
+        this.select.emit(this.id);
+    }
+
+    // Signals approach
+    // avatar = input.required<string>();
+    // name = input.required<string>();
+
+    // imagePath = computed(() => {
+    //     return 'assets/users/' + this.avatar();
+    // })
+
+}
