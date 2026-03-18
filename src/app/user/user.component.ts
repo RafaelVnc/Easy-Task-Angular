b2fca768ef1986d998cf724094bf0ed2f577d277
Rafael Venancio <rafael.venancio08.06@gmail.com>
2026-03-18 14:19:46 -0300

    Tasks component and user component refactor

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
 
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -9,17 +10,16 @@ import { Component, computed, EventEmitter, Input, input, Output } from '@angula
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-    @Input({ required: true }) id!: string;
-    @Input({ required: true }) avatar!: string;
-    @Input({ required: true }) name!: string;
+    @Input({ required: true }) user!: User;
+    @Input({ required: true }) selected!: Boolean;
     @Output() select = new EventEmitter<string>(); // Pass the information that a specific user was selected to the component that is using UserComponent
 
     get imagePath() {
-        return 'assets/users/' + this.avatar;
+        return 'assets/users/' + this.user.avatar;
     }
   
     onSelectUser() {
-        this.select.emit(this.id);
+        this.select.emit(this.user.id);
     }
 
     // Signals approach
